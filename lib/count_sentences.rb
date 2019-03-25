369ebe7829b771eff6a8e1aa013fddfbf94c8cf6
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-25 10:12:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,12 @@ require 'pry'
 class String
 
   def sentence?
-    
+    @sentence = sentence 
+    if sentence.end_with?(".")
+      true 
+    else 
+      false 
+    end
   end
 
   def question?
